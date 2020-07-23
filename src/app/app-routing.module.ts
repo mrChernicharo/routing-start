29394b0f3d27efcfde8021dcf1d1623e9e9452aa
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-23 12:40:57 -0300

    canDeactivate run code on leave route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent},
-      { path: ':id/edit', component: EditServerComponent},
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ]
   },
   { path: 'users', component: UsersComponent,
